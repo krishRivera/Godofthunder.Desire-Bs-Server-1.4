6d03a02c80679a61c07cbd4c2309640b1c74234b
Vivek Tiwari <112796674+Sudo-Desier@users.noreply.github.com>
2022-10-13 22:30:24 +0530

    Add files via upload

--- Desire Server/config.py
+++ Desire Server/config.py
@@ -1,8 +1,13 @@
 # place any of your own overrides here.
 # see bombsquad_server for details on what you can override
 # examples (uncomment to use):
-config['partyName'] = u'partyname'
+config['partyName'] = u'\U0001F329StormX Epic TeaMs\U0001F329'
 config['sessionType'] = 'teams'
 config['maxPartySize'] = 9
 config['port'] = 43210
 config['playlistCode'] = 383245
+config['partyIsPublic'] = True
+config['playlistShuffle'] = True
+config['autoBalanceTeams'] = True
+config['teamsSeriesLength'] = 7
+config['ffaSeriesScoreToWin'] = 24
