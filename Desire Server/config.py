11823e0555b7daf48e0a31bb57aeb20e56f35715
Vivek Tiwari <112796674+Sudo-Desier@users.noreply.github.com>
2022-10-13 22:31:16 +0530

    Update config.py

--- Desire Server/config.py
+++ Desire Server/config.py
@@ -1,7 +1,7 @@
 # place any of your own overrides here.
 # see bombsquad_server for details on what you can override
 # examples (uncomment to use):
-config['partyName'] = u'\U0001F329StormX Epic TeaMs\U0001F329'
+config['partyName'] = u'Server Name'
 config['sessionType'] = 'teams'
 config['maxPartySize'] = 9
 config['port'] = 43210
