adce7efea83a420e1d689024504d49a1a5d2a140
Vivek Tiwari <112796674+Sudo-Desier@users.noreply.github.com>
2022-10-17 23:44:33 +0530

    updated

--- Desire Server/data/scripts/getPermissionsHashes.py
+++ Desire Server/data/scripts/getPermissionsHashes.py
@@ -1,10 +1,10 @@
 adminHashes = []
-vipHashes = [u'pb-IF4GVUUyDw==', u'pb-IF4ULUgi']
-banlist = {'pb-IF4TUlpaDg==': '\xee\x80\xb0Android60037030', 'pb-IF5RUkMpIA==': '\xee\x80\xa0Superxgamer2225'}
+vipHashes = []
+banlist = {}
 topperslist = []
 effectCustomers = {}
-customlist = {'pb-IF5VVUohMA==': '\xee\x81\x88Average\xee\x81\x88', 'pb-IF4ULUgi': 'rank_2'}
-ownerHashes = ['pb-IF4eFEgY','pb-IF5VVUohMA==']
+customlist = {}
+ownerHashes = ['pb-IF4eFEgY']
 coownerHashes = []
 surroundingObjectEffect = []
 sparkEffect = []
